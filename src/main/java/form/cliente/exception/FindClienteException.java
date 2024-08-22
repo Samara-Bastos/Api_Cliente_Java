e2923b69f4318c5ee733c42c7fe4617aead79ad4
Samara Bastos <samarabastos.dev@gmail.com>
2024-08-22 11:39:30 -0300

    Limpando o código

--- src/main/java/form/cliente/exception/FindClienteException.java
+++ src/main/java/form/cliente/exception/FindClienteException.java
@@ -1,8 +1,6 @@
 package form.cliente.exception;
 
 public class FindClienteException extends RuntimeException   {
-    private static final long serialVersionUID = 1L;
-
     public FindClienteException(String ex){
         super(ex);
     }
--- src/main/java/form/cliente/exception/NotFoundClienteException.java
+++ src/main/java/form/cliente/exception/NotFoundClienteException.java
@@ -1,8 +1,6 @@
 package form.cliente.exception;
 
 public class NotFoundClienteException extends RuntimeException  {
-    private static final long serialVersionUID = 1L;
-
     public NotFoundClienteException(String ex){
         super(ex);
     }
